eefc8fd511437bd603faa825fc544c7eeba2191d
Marcos <HI-18@LAPTOP-1FEVPPQ6>
2016-09-11 18:15:53 -0500

    ComponentType,EquipmentType

---
+++ INEQ/INEQ/Models/ComponentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INEQ.Models
+{
+    public class ComponentType
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+
+        public virtual ICollection<Component> Component { get; set; }
+
+    }
---
+++ INEQ/INEQ/Models/EquipmentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INEQ.Models
+{
+    public class EquipmentType
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string UsefulLife { get; set; }
+        public string GuaranteeDuration { get; set; }
+        public bool Active { get; set; }
+
+        public virtual ICollection<Equipment> Equipment { get; set; }
+
+    }
+}

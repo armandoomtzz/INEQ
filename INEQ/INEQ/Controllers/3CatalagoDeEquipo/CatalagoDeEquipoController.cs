e725b21d1e98e5453ccdcc388c8ea7399378000b
Marcos <HI-18@LAPTOP-1FEVPPQ6>
2016-09-11 21:41:42 -0500

    caso 3 incompleto

---
+++ INEQ/INEQ/Controllers/3CatalagoDeEquipo/CatalagoDeEquipoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INEQ.Controllers
+{
+   
+        public class CatalagoDeEquipoController : Controller
+        {
+        public ActionResult Index()
+        {  
+            ViewData["Descripcion"] = "valor";
+            ViewData["vida util"] = "valor";
+            ViewData["]Duracion de garantia"] = "valor";
+            ViewData["activo"] = "valor";
+            return View();
+        }
+    }
+    }
+}
---
+++ INEQ/INEQ/Models/3CatalagoDeEquipo/CatalagoDeEquipo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+
+namespace INEQ.Models.CatalagoDeEquipo
+{
+    public class CatalagoDeEquipo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string UsefulLife { get; set; }
+        public string GuaranteeDuration { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
+}
